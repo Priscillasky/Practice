baae5dc628072a2336a4d7964c7abc7af4d14f01
Priscillasky <priscillasky542@gmail.com>
2023-06-27 19:16:00 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Content from './Content'
+import Employees from './Employees'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Employees />
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
